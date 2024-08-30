d1330f716e2f10f0a3e00dc650837201139f5b6f
unknown <pavlus131@gmail.com>
2024-08-30 09:57:20 +0200

    produkty

--- index.php
+++ index.php
@@ -1,3 +1,138 @@
 <?php
-
+include_once "config.php";
 echo"ahoj eshop";
+
+$sql = "SELECT * FROM `prokukt`";
+
+      
+$result = mysqli_query($db,$sql);
+
+$produkty=mysqli_fetch_assoc($result);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Header and Footer</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- FontAwesome for icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">YourBrand</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Domov</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">O nás</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Produkty</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-shopping-cart"></i> <!-- Shop Icon -->
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-user"></i> <!-- Account Icon -->
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Content -->
+    <div class="container mt-5">
+        <!-- Your content will go here -->
+
+    <?php
+
+    while ($produkt = mysqli_fetch_assoc($result)) {
+    var_dump($produkt);
+        echo $produkt["meno"];
+    }
+
+    ?>
+
+    </div>
+
+    <!-- Footer -->
+    <footer class="bg-light text-center text-lg-start">
+        <div class="container p-4">
+            <div class="row">
+                <!-- Contact Information -->
+                <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Contact Us</h5>
+                    <p>
+                        <i class="fas fa-phone-alt"></i> +123 456 7890<br>
+                        <i class="fas fa-envelope"></i> contact@yourbrand.com<br>
+                        <i class="fas fa-map-marker-alt"></i> 123 Your Street, Your City, Your Country
+                    </p>
+                </div>
+
+                <!-- Navigation Links -->
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Quick Links</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#" class="text-dark">Domov</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-dark">O nás</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-dark">Produkty</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-dark">Shop</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-dark">Account</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Social Media Icons -->
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Follow Us</h5>
+                    <a href="#" class="text-dark mr-2">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" class="text-dark mr-2">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" class="text-dark mr-2">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="#" class="text-dark">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="text-center p-3 bg-dark text-white">
+            © 2024 YourBrand. All rights reserved.
+        </div>
+    </footer>
+
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
